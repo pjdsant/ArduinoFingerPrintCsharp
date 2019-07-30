f8901f71384d0dc2dc078479f29ac7e27a5e77f9
Paulo <pjsantoss@hotmail.com>
2019-07-29 22:06:15 -0300

    Initial Source Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace YouPlay
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.textBoxReceive = new System.Windows.Forms.TextBox();
+            this.timerCOM = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(31, 38);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(168, 45);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "&Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.BtnConnect_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(234, 45);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(320, 33);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(31, 116);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(168, 43);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "&Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // textBoxSend
+            // 
+            this.textBoxSend.Location = new System.Drawing.Point(234, 122);
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.Size = new System.Drawing.Size(662, 31);
+            this.textBoxSend.TabIndex = 3;
+            // 
+            // textBoxReceive
+            // 
+            this.textBoxReceive.Location = new System.Drawing.Point(31, 198);
+            this.textBoxReceive.Multiline = true;
+            this.textBoxReceive.Name = "textBoxReceive";
+            this.textBoxReceive.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxReceive.Size = new System.Drawing.Size(865, 308);
+            this.textBoxReceive.TabIndex = 4;
+            // 
+            // timerCOM
+            // 
+            this.timerCOM.Enabled = true;
+            this.timerCOM.Interval = 1000;
+            this.timerCOM.Tick += new System.EventHandler(this.TimerCOM_Tick);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SerialPort1_DataReceived);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(1003, 45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(390, 461);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Resize += new System.EventHandler(this.PictureBox1_Resize);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1073, 556);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(247, 72);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1442, 660);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxReceive);
+            this.Controls.Add(this.textBoxSend);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox textBoxSend;
+        private System.Windows.Forms.TextBox textBoxReceive;
+        private System.Windows.Forms.Timer timerCOM;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace YouPlay
+{
+    public partial class Form1 : Form
+    {
+
+        string RxString;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void atualizaListaCOMs()
+        {
+            int i;
+
+            bool differentAmount;
+
+            i = 0;
+            differentAmount = false;
+
+            if (comboBox1.Items.Count == SerialPort.GetPortNames().Length)
+            {
+                foreach (string s in SerialPort.GetPortNames())
+                {
+                    if (comboBox1.Items[i++].Equals(s) == false)
+                    {
+                        differentAmount = true;
+                    }
+                }
+            }
+            else
+            {
+                differentAmount = true;
+            }
+
+            if (differentAmount == false)
+            {
+                return;                     //retorna
+            }
+
+            comboBox1.Items.Clear();
+
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+            }
+
+            comboBox1.SelectedIndex = 0;
+
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TimerCOM_Tick(object sender, EventArgs e)
+        {
+            atualizaListaCOMs();
+        }
+
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == false)
+            {
+                try
+                {
+                    serialPort1.PortName = comboBox1.Items[comboBox1.SelectedIndex].ToString();
+                    serialPort1.Open();
+
+                }
+                catch
+                {
+                    return;
+
+                }
+                if (serialPort1.IsOpen)
+                {
+                    btnConnect.Text = "Disconnect";
+                    comboBox1.Enabled = false;
+
+                }
+            }
+            else
+            {
+
+                try
+                {
+                    serialPort1.Close();
+                    comboBox1.Enabled = true;
+                    btnConnect.Text = "Connect";
+                }
+                catch
+                {
+                    return;
+                }
+
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Load("C:\\repositorio\\bio.jpeg");
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort1.IsOpen == true)  // se porta aberta
+                serialPort1.Close();         //fecha a porta
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen == true)          //porta está aberta
+                serialPort1.Write(textBoxSend.Text);
+        }
+
+        private void SerialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
+            pictureBox1.InitialImage = null;
+
+            RxString = serialPort1.ReadExisting();              //le o dado disponível na serial
+
+            this.Invoke(new EventHandler(trataDadoRecebido));
+
+            if (RxString == "2000" || RxString == "1000")
+            {
+                pictureBox1.Load("C:\\repositorio\\perfilfoto.jpg");
+            }
+            //this.Invoke(new EventHandler(trataDadoRecebido));   //chama outra thread para escrever o dado no text box
+        }
+
+        private void trataDadoRecebido(object sender, EventArgs e)
+        {
+            pictureBox1.Load("C:\\repositorio\\bio.jpeg");
+
+            textBoxReceive.AppendText(RxString);
+
+            //if (RxString.Contains("#1") || RxString.Contains("#1"))
+            //{
+           // pictureBox1.Load("C:\\repositorio\\perfilfoto.jpg");
+            //}
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            // pictureBox1.Load("C:\\repositorio\\perfilfoto.jpg");
+
+            // pictureBox1.Image = null;
+
+            pictureBox1.Load("C:\\repositorio\\bio.jpeg");
+
+        }
+
+        private void PictureBox1_Resize(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
